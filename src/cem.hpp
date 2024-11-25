1d93c279387231a5a2ae1ca49dc9df72e38c75c6
Sephixum <emotive.5353@gmail.com>
2024-11-25 13:26:59 +0330

    Add quadratic solver

--- src/cem.hpp
+++ src/cem.hpp
@@ -35,6 +35,7 @@ consteval auto abs(numeric_t auto value) noexcept {
     return value < 0 ? -value : value;
 }
 
+template <std::size_t max_iter = 10>
 consteval auto sqrt(const floating_t auto x) noexcept {
     using type_t = decltype(x);
 
@@ -49,10 +50,10 @@ consteval auto sqrt(const floating_t auto x) noexcept {
     auto guess = x / 2.0;
 
     const auto relative_epsilon = std::numeric_limits<type_t>::epsilon() * x;
-    constexpr static const auto absolute_epsilon =
+    static constexpr const auto absolute_epsilon =
         std::numeric_limits<type_t>::epsilon();
 
-    for ([[maybe_unused]] auto i : std::views::iota(0, 10)) {
+    for (std::size_t i{0}; i < max_iter; ++i) {
         auto new_guess = (guess + x / guess) / 2.0;
         auto guess_abs_diff = abs(new_guess - guess);
         auto max_of_epsilon = max(absolute_epsilon, relative_epsilon);
@@ -72,7 +73,7 @@ consteval auto Q_rsqrt(const floating_t auto x) noexcept {
     static constexpr type_t half = 0.5;
     static constexpr type_t three_halfs = 0.5 * 3;
 
-    constexpr static const auto magic = std::invoke([]() {
+    static constexpr const auto magic = std::invoke([]() {
         if constexpr (std::same_as<type_t, const float>) {
             return std::uint32_t{0x5f3759df};
         } else if constexpr (std::same_as<type_t, const double>) {
@@ -102,6 +103,26 @@ consteval auto are_approximately_equal(const numeric_t auto computed,
                                        const numeric_t auto expected,
                                        auto epsilon = 1e-2) noexcept {
     return abs(computed - expected) <= epsilon;
+}
+
+consteval auto quadratic_results(floating_t auto a, floating_t auto b,
+                                 floating_t auto c) {
+    using common_t = decltype(a + b + c);
+
+    if (a == common_t{0}) {
+        return std::tuple{false, common_t{0}, common_t{0}};
+    }
+
+    common_t discrminant = b * b - static_cast<common_t>(4) * a * c;
+    if (discrminant < 0) {
+        return std::tuple{false, common_t{0}, common_t{0}};
+    }
+
+    common_t sqrt_discrminant = sqrt(static_cast<double>(discrminant));
+    common_t root1 = (-b + sqrt_discrminant) / (static_cast<common_t>(2) * a);
+    common_t root2 = (-b - sqrt_discrminant) / (static_cast<common_t>(2) * a);
+
+    return std::tuple{false, root1, root2};
 }
 
 } // namespace cem
